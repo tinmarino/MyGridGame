caef32601bc0619eebacc65eb838e26110bc8c20
tinmarino <tinmarino@gmail.com>
2019-11-20 15:33:15 -0300

    Html full screen (keeping 480/320 = 1.5 aspect ratio)

--- html/src/com/mygdx/game/client/HtmlLauncher.java
+++ html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -1,22 +1,39 @@
 package com.mygdx.game.client;
 
+// Import Gdx
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.utils.Global.Global;
 
+// Import Html
+import com.google.gwt.user.client.Window;
+
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(320, 480);
-		Global.platformOs = new PlatformHtml(); 
-		return cfg; 
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig () {
+        // Get client size
+        int height = Window.getClientHeight();
+        int width = Window.getClientWidth();
 
-        @Override
-        public ApplicationListener getApplicationListener () {
-                return new MyGdxGame();
-        }
+        // Get desired size
+        int width_desired = (int) (0.95 * Math.min(width, 0.67 * height));
+        int height_desired = (int) (0.95 * Math.min(height, 1.5 * width));
+
+        // Configure size
+        GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(width_desired, height_desired);
+
+        // Create Global platform class
+        Global.platformOs = new PlatformHtml();
+
+        // Return cfg
+        return cfg;
+    }
+
+    @Override
+    public ApplicationListener getApplicationListener () {
+            return new MyGdxGame();
+    }
 }
--- html/src/com/mygdx/game/client/PlatformHtml.java
+++ html/src/com/mygdx/game/client/PlatformHtml.java
@@ -1,24 +1,52 @@
+/*
+    Handle platform specific action for HTML
+
+    Screen horizontal or vertical, all is calculated for a 480 / 320 = 1.5 ration
+        Same equation as in HtmlLauncher
+*/
+
 package com.mygdx.game.client;
 
+// Import Gdx
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.mygdx.game.utils.Global.PlatformOs;
+
+// Import Html
+import com.google.gwt.user.client.Window;
+import java.lang.Math;
 
 public class PlatformHtml implements PlatformOs {
 
 @Override
 public void setOrientation(Orientation orientation) {
+    int width_desired = 320;
+    int height_desired = 320;
+
     // Clear screeen (hopefully)
     // (Not necessary) Gdx.gl.glClearColor(0, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+    // Get client size
+    int height = Window.getClientHeight();
+    int width = Window.getClientWidth();
+
+
     if (orientation == Orientation.PAYSAGE){
-        // x, y, boolean for fullscreen yes or no 
-        Gdx.graphics.setDisplayMode(480,320,false);
+        // Get desired size
+        width_desired = (int) (0.95 * Math.min(width, 1.5 * height));
+        height_desired = (int) (0.95 * Math.min(height, 0.67 * width));
+
     }
     if (orientation == Orientation.PORTRAIT){
-        Gdx.graphics.setDisplayMode(320,480,false);
+        // Get desired size
+        width_desired = (int) (0.95 * Math.min(width, 0.67 * height));
+        height_desired = (int) (0.95 * Math.min(height, 1.5 * width));
     }
+
+    // Change size
+    // x, y, boolean for fullscreen yes or no
+    Gdx.graphics.setDisplayMode(width_desired, height_desired, false);
 
 }
 
